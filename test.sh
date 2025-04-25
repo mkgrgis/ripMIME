d7b05f9f242453d37fca0e21ba4e73ee10106a83
mkgrgis <pgsqlitegis@tutamail.com>
2025-04-25 16:05:08 +0300

    Add tests

---
+++ test.sh
@@ -0,0 +1,30 @@
+d=$(dirname "$0");
+d=$(readlink -f "$d");
+
+tmpdir='/tmp';
+make clean;
+make;
+[ ! -f "$d/ripmime" ] && exit;
+[ -d "$tmpdir/ripmime" ] && rm -r "$tmpdir/ripmime";
+mkdir "$tmpdir/ripmime";
+tmpdir="$tmpdir/ripmime";
+err=0
+
+ls -1 "$d/test_data" | while read t; do
+	dt="$tmpdir/$t";
+	mkdir "$dt" "${dt}_res" -q;
+	cd "$dt";
+	"$d/ripmime" -i "$d/test_data/$t" -d "$dt" -v > "${dt}_res/output0";
+	ls -1 "$dt" | while read t1; do
+		sha512sum "$dt/$t1" >> "${dt}_res/sha512sum";
+	done;
+	diff "$d/expected/${t}_res/output0" "${dt}_res/output0";
+	od=$?;
+	[ $od != 0 ] && err=$(($err +1));
+	diff "$d/expected/${t}_res/sha512sum" "${dt}_res/sha512sum";
+	sd=$?;
+	[ $sd != 0 ] && err=$(($err +1));
+done;
+
+[ $err == 0 ] && echo "no errors";
+[ $err != 0 ] && echo "THERE WAS $err ERRORS";
